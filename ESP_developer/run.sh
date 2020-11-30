5d33d5a58a15a9f28102a2ffa4957cb175a60ea4
axcxl <axcxl.dev@gmail.com>
2020-11-30 17:33:05 +0200

    ESP dev: Clean-up dockerfile and run script

--- ESP_developer/run.sh
+++ ESP_developer/run.sh
@@ -16,21 +16,25 @@ WORKSPACE=$1
 
 if [[ "$(docker images -q $IMG_NAME:$VERS 2> /dev/null)" == "" ]]; then
     echo "BUILDING using IDE version $VERS"
-    docker build -t $IMG_NAME:$VERS --build-arg VERS=$VERS .
-else
-    echo "STARTING, workspace at $1"
-    docker run \
-        -it \
-        --rm \
-        --network=host \
-        --privileged \
-        -e DISPLAY=$DISPLAY \
-        -v $SERIAL:$SERIAL \
-        -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v /dev:/dev \
-        -v $WORKSPACE:/home/$USER/workspace \
-        --name arduino \
-        $IMG_NAME:$VERS \
-        arduino
+    docker build -t $IMG_NAME:$VERS --build-arg VERS=$VERS --build-arg USER=$USER .
+
+    if [[ "$?" != "0" ]]; then
+        echo "BUILD FAILED!"
+        exit -1
+    fi
 fi
 
+echo "STARTING, workspace at $1"
+docker run \
+    -it \
+    --rm \
+    --network=host \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v $SERIAL:$SERIAL \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /dev:/dev \
+    -v $WORKSPACE:/home/$USER/workspace \
+    --name $IMG_NAME \
+    $IMG_NAME:$VERS \
+    arduino
